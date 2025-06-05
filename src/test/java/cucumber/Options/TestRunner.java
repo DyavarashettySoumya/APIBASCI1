53ee375d6f14324290bad8adcd9231e5f0cbd229
DyavarashettySoumya <soumyadt26@gmail.com>
2025-06-05 18:40:14 +0530

    reports generation

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,15 +5,8 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {
-                "pretty",
-                "json:target/cucumber.json"  // Required for HTML report
-        },
-        features = "src/test/java/features",
-        glue = "stepDefinitions",
-        tags = "@Smoke"  // Optional: Filter tests
-)
+@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json"
+        ,glue= {"stepDefinations"})
 public class TestRunner {
 //tags= {"@DeletePlace"}  compile test verify
 
